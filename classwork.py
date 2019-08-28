c718a7b385277883b73499758af3f5783c71de4e
autumn-ragland <raglanda998@gmail.com>
2019-08-28 10:20:42 -0500

    Graded 08-28-2019 10:19:04

--- classwork.py
+++ classwork.py
@@ -1,5 +1,4 @@
 person_array = ["Kenn", "Kevin", "Erin", "Autumn"]
-
 def stupid_array_tricks(array):
         print(array[1:3])
         print(array[2:4])
@@ -7,8 +6,6 @@ def stupid_array_tricks(array):
         print(array)
         array.pop(1)
         print(array)
-
-
 (stupid_array_tricks(person_array))
 
 #Problem 2
@@ -19,19 +16,15 @@ while(user!="q"):
 #Problem 3
 data=['GOOD_DATA','DECENT_DATA','BAD_DATA','DECENT_DATA','GOOD_DATA','BAD_DATA','GOOD_DATA','DECENT_DATA','BAD_DATA',
       'GOOD_DATA']
-
 print(data.__len__())
-data.pop(2)
-data.pop(4)
-data.pop(6)
+data.pop(2) # !! : you should be able to do this without knowing the index position!
+data.pop(4) # !! : you should be able to do this without knowing the index position!
+data.pop(6) # !! : you should be able to do this without knowing the index position!
 print(data)
 print(data.__len__())
 
 #Problem 4
 score_list = [21,14,6,8,28,42,21]
-
 print(f"The sum of the numbers is {sum(score_list)}")
-
 print(f"The max of the numbers is {max(score_list)}")
-
 print(f"The min of the numbers is {min(score_list)}")
